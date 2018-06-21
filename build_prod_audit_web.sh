f42405e44511f696607b80d05dfe54571f765462
Max Kaye <m@xk.io>
2018-06-21 16:34:45 +1000

    Add sneaky output cache for netlify builds

--- build_prod_audit_web.sh
+++ build_prod_audit_web.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+OUTPUT_CACHE="./node_modules/__cache__output"
+
 # on netlify npm removes all other packages when installing bower...
 # netlify sample: > added 1 package and removed 507 packages in 6.684s
 yarn install
 
-if [ -n "$BRANCH" ] && [ "$BRANCH" -eq "master" ]; then 
+if [ -n "$BRANCH" ] && [ "$BRANCH" -eq "master" ]; then
   bower cache clean
   rm -rf bower_components
   bower install
 fi
 
+if [ -d "$OUTPUT_CACHE" ]; then
+  cp -a "$OUTPUT_CACHE" ./output
+fi
+
 yarn audit-prod-web
 
+rm -rf "$OUTPUT_CACHE"
+cp -a ./output "$OUTPUT_CACHE"
